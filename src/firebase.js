ab732452cd63809ba86ffb71224f1484fd85d616
Cory Risch-Smith <Corys_Macbook_Pro@devcor-MacBook-Pro.local>
2020-09-21 15:44:39 -0700

    initial project upload

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBawTwJ0Z2_ehUOPIHmNAOwpcG0R2tSRzk",
+  authDomain: "whatsapp-mern-rebuild.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-rebuild.firebaseio.com",
+  projectId: "whatsapp-mern-rebuild",
+  storageBucket: "whatsapp-mern-rebuild.appspot.com",
+  messagingSenderId: "1006593303076",
+  appId: "1:1006593303076:web:021a2f6cfa93ee9ec6aeb1",
+  measurementId: "G-6G4R887Q1L",
+};
